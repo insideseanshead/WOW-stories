d58a2d96f3aa20001d5fd9141419dc0c4b2f1914
Sean Morgan <insideseanshead@gmail.com>
2024-09-27 15:18:34 -0700

    added function to change question pages.

--- javascript.js
+++ javascript.js
@@ -8,4 +8,20 @@ formEl.addEventListener('submit', (event) => {
 
   console.log(formEl.id);
   localStorage.setItem(`${formEl.id}`, JSON.stringify(data));
+
+  nextPage();
 });
+
+const nextPage = function () {
+  const currentURL = window.location.href;
+  let questionNumber = Number(currentURL.charAt(currentURL.length - 6));
+
+  if (questionNumber < 8) {
+    window.location = `./thank-you.html`;
+    console.log(questionNumber + 1);
+
+    window.location = `./question-${questionNumber + 1}.html`;
+  } else {
+    window.location = `./thank-you.html`;
+  }
+};
